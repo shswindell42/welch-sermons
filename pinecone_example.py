e34771b1359a6445b9541bbbce96edd059e82fb4
Spencer Swindell <spencer.swindell@gmail.com>
2023-09-18 21:08:12 -0500

    example of querying pinecone

--- pinecone_example.py
+++ pinecone_example.py
@@ -1,7 +1,16 @@
 import pinecone
 import os
+from sentence_transformers import SentenceTransformer
 
 api_key = os.environ.get("PINECONE_API_KEY")
 env = os.environ.get("PINECODE_ENV")
 pinecone.init(api_key=api_key, environment=env)
-print(pinecone.list_indexes())
+index = pinecone.Index("sermons")
+
+query = "What is the fruit of the spirit?"
+model = SentenceTransformer('all-MiniLM-L6-v2')
+query_vector = model.encode(query)
+
+results = index.query(query_vector.tolist(), top_k=10, include_metadata=True)
+
+print(results)
