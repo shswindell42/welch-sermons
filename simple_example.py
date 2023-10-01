31b0fed6dfaf7b7aa52ca6ee1d9d71d8039f7102
Spencer Swindell <spencer.swindell@gmail.com>
2023-10-01 16:42:24 -0500

    added example of similar sentence

--- simple_example.py
+++ simple_example.py
@@ -1,13 +1,19 @@
 from sentence_transformers import SentenceTransformer, util
 
-v_text = 'The cat jumped over the fence'
-u_text = 'This is a great presentation'
+original_text = 'The cat jumped over the fence'
+different_text = 'This is a great presentation'
+similar_text = 'The cat jumped over the wall'
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
-v = model.encode(v_text)
-u = model.encode(u_text)
+v = model.encode(original_text)
+u = model.encode(different_text)
+z = model.encode(similar_text)
 sim = util.cos_sim(v, u)
+dif = util.cos_sim(v, z)
 
-print(f"{v_text} -> {v[0:5]}")
-print(f"{u_text} -> {u[0:5]}")
+print(f"{original_text} -> {v[0:5]}")
+print(f"{different_text} -> {u[0:5]}")
 print(f"Cosine similarity -> {sim.item()}")
+
+print(f"{similar_text} -> {z[0:5]}")
+print(f"Cosine similarity -> {dif.item()}")
